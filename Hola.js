8210db7225d8a2abfb647ee96cfc58198d596e1d
pbarco <pablotomasbarco@gmail.com>
2019-05-04 14:08:46 -0300

    Renderizando

---
+++ Hola.js
@@ -0,0 +1,9 @@
+import React from 'hola'
+class Hola extends React.Component {
+
+  render(){
+    return <div> Hola {this.props.nombre}!!!</div>
+  }
+}
+
+export default Hola
--- app.jsx.js
+++ app.jsx.js
@@ -1,9 +1,4 @@
-let Hola = class Hola extends React.Component {
-
-  render(){
-    return <div> Hola {this.props.nombre}!!!</div>
-  }
-}
+import Hola from "Hola"
 
 ReactDOM.render(<Hola nombre ="JSX" />,
   document.querySelector("#app")
