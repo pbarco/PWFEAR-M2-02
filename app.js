bfa10d47bb556efb987f572fb2d1871f51e49fdc
pbarco <pablotomasbarco@gmail.com>
2019-05-04 14:12:22 -0300

    Pusheando

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let hola = class Hola extends React.Component {
   render(){
     //React.createElement(Etiquete, Atributos, Contenido, Propiedades del Objeto)
     return etiqueta = React.createElement("div", null, "Hola ", this.props.nombre)
+    return etiqueta
   }
 
 }
 ///////////////////////////////////////////////////////////////////////////////
 //ReactDom.render(COMPONENTE, AREA)
+let app= document.querySelector("#app")
+let saludo = React.createElement(hola, { nombre : "React"})
 
-ReactDOM.render(React.createElement(hola, { nombre : "React"}),
-document.querySelector("#app"))
+ReactDOM.render(saludo, app)
