acb34466e6e47545ef41908802aeb6b076bc8660
pbarco <pablotomasbarco@gmail.com>
2019-05-18 12:00:20 -0300

    Creando componente Google Maps

--- Componentes-React/app.js
+++ Componentes-React/app.js
@@ -10,17 +10,35 @@ let hola = class Hola extends React.Component {
 
 }
 
-  let Imagen = class Imagen extends React.Component {
+let Imagen = class Imagen extends React.Component {
     render(){
       return React.createElement("img", { src : this.props.url}, null)
     }
 
   }
+
+let GoogleMap = class GoogleMap extends React.Component {
+
+  render(){
+    const API_KEY = 
+    let lat = this.props.lat
+    let long = this.props.long
+    let zoom = this.props.zoom
+
+    let url = `https://www.google.com/maps/embed/v1/view?key=${API_KEY}&center=${lat},${long}&zoom=${zoom}&maptype=satellite`
+
+    return React.createElement("iframe", {src : url, frameBorder : 0, width : 600, height : 450}, null)
+  }
+
+}
+
+
 ///////////////////////////////////////
 //ReactDOM.render(COMPONENTE, AREA)
 
 ReactDOM.render(
   //React.createElement(hola, { nombre : "Luna" }),
-  React.createElement(Imagen, {url : "https://lorempixel.com/100/100/animals"}),
+  //React.createElement(Imagen, {url : "https://lorempixel.com/100/100/animals"}),
+  React.createElement(GoogleMap, {lat: -34.6058437 , long: -58.3794516  , zoom:19.5}),
   document.querySelector("#app")
 )
